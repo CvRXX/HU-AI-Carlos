698670e48181d9d29d6b07cde8ce3d5a3c22c1a3
Carlos van Rooijen <info@carlosvanrooijen.nl>
2020-02-18 19:52:28 +0100

    adding assignment 2

---
+++ k-means/k-means.py
@@ -0,0 +1,139 @@
+import numpy as np
+import weatherMeasurement
+from collections import Counter
+import random
+import matplotlib.pyplot as pyplot
+
+data = np.genfromtxt('dataset1.csv', delimiter=';', usecols=[0,1,2,3,4,5,6,7], converters={5: lambda s: 0 if s == b"-1" else float(s), 7: lambda s: 0 if s == b"-1" else float(s)})
+validationData = np.genfromtxt('validation1.csv', delimiter=';', usecols=[0,1,2,3,4,5,6,7], converters={5: lambda s: 0 if s == b"-1" else float(s), 7: lambda s: 0 if s == b"-1" else float(s)})
+days = np.genfromtxt('days.csv', delimiter=';', usecols=[0,1,2,3,4,5,6,7], converters={5: lambda s: 0 if s == b"-1" else float(s), 7: lambda s: 0 if s == b"-1" else float(s)})
+
+measurements = []
+for item in data :
+	measurment = weatherMeasurement.WeatherMeasurement(*item[1:])
+	measurment.setDate(item[0])
+	measurements.append(measurment)
+
+validationMeasurementsWithoutDate = []
+for item in validationData :
+	validationMeasurementsWithoutDate.append(weatherMeasurement.WeatherMeasurement(*item[1:]))
+
+validationMeasurementsWithDate = []
+for item in validationData :
+	measurment = weatherMeasurement.WeatherMeasurement(*item[1:])
+	measurment.setDate(item[0])
+	validationMeasurementsWithDate.append(measurment)
+
+def kmeans(dataset,k):
+	centroids = random.sample(dataset, k)
+	
+	hasSomethingChanged = 1
+	for run in range(0,10000):
+		if hasSomethingChanged == 0:
+			break
+		hasSomethingChanged = 0
+
+		centroidSums = [weatherMeasurement.WeatherMeasurement(0,0,0,0,0,0,0)] * k
+		centroidNumberOfItems = [0] * k
+		# Find the closest centroid for each item in the dataset and assign it to the item.
+		for item in dataset:
+			closestCentroid = 0
+			closestCentroidDistance = centroids[0].measureDistance(item)
+			for idx, centroid in enumerate(centroids[1:]):
+				distance = centroid.measureDistance(item)
+				if distance < closestCentroidDistance:
+					closestCentroid = idx+1
+					closestCentroidDistance = distance
+			if item.centroid != closestCentroid:
+				hasSomethingChanged+=1
+				item.centroid = closestCentroid
+			centroidSums[closestCentroid]+=item
+			centroidNumberOfItems[closestCentroid]+=1
+
+
+		for centroidIdx, centroidSum in enumerate(centroidSums):
+			if centroidNumberOfItems[centroidIdx]:
+				centroids[centroidIdx] = weatherMeasurement.WeatherMeasurement(
+					centroidSum.fg/centroidNumberOfItems[centroidIdx],
+					centroidSum.tg/centroidNumberOfItems[centroidIdx],
+					centroidSum.tn/centroidNumberOfItems[centroidIdx],
+					centroidSum.tx/centroidNumberOfItems[centroidIdx],
+					centroidSum.sq/centroidNumberOfItems[centroidIdx],
+					centroidSum.dr/centroidNumberOfItems[centroidIdx],
+					centroidSum.rh/centroidNumberOfItems[centroidIdx])
+	return(centroids,dataset)
+
+
+def calculateDistance(centroids,dataset):	
+	for idx, centroid in enumerate(centroids):
+		centroidSum=0
+		centroidItems=0
+		for item in dataset:
+			if idx == item.centroid:
+				centroidItems+=1
+				centroidSum+=item.measureDistance(centroids[idx])
+		if centroidItems:
+			centroids[idx].centroidDistance = (centroidSum/centroidItems)
+		else:
+			print("this happens")
+	totalSum = 0		
+	for centroid in centroids:
+		totalSum += centroid.centroidDistance
+	return totalSum/len(centroids)
+
+def findCentroidLabels(centroids,dataset):
+	for idx, centroid in enumerate(centroids):
+		labels = {}
+		for item in dataset:
+			if idx == item.centroid:
+				if item.label in labels:
+					labels[item.label]+=1
+				else:
+					labels[item.label]=1
+		
+
+		centroid.label=max(labels, key=labels.get)
+	return centroids, dataset
+
+
+
+
+
+def findBestK(dataset, maxK, runs):
+	kScores = []
+	for k in range(2,maxK+1):
+		sum = 0
+		for run in range(1,runs+1):
+			print("Finding best K: " + str(k) + "/" + str(maxK) + " run: " + str(run) + "/" + str(runs) + "              ", end="\r")
+			centroids, dataset = kmeans(dataset,k)
+			sum += calculateDistance(centroids,dataset)
+		kScores.append(sum/runs)
+	pyplot.scatter(range(2,maxK+1),kScores,label='Scatter Plot 1',color='r')
+	pyplot.show()
+	return kScores	
+
+def validateLabels(dataset, k):
+	good = 0
+	false = 0
+	centroids, dataset = findCentroidLabels(*kmeans(dataset,k))
+	for idx, centroid in enumerate(centroids):
+		for item in dataset:
+			if item.centroid == idx:
+				if item.label == centroid.label:
+					good+=1
+				else:
+					false+=1
+	print("Good: " + str(good))
+	print("False: " + str(false))
+	print("That's: " + str(round(good/((false+good)/100 )) ) + "%!")
+
+
+
+
+findBestK(measurements, 10, 50)
+# From the graph I can tell the best K is: 5!
+validateLabels(measurements,5)
+# I can validate around 50% of the labels correctly. 
+# This is not so bad if you compare this 
+# to k-neigbours which should be better at this!
+
---
+++ k-means/weatherMeasurement.py
@@ -0,0 +1,116 @@
+import numpy
+import random
+from scipy.spatial import distance
+
+class WeatherMeasurement:
+   'Base class for datapoints'
+   date = None
+   label = None
+   centroid = None
+   centroidDistance = None
+
+   def __init__(self, fg, tg, tn, tx, sq, dr, rh):
+      self.fg = fg
+      self.tg = tg
+      self.tn = tn
+      self.tx = tx
+      self.sq = sq
+      self.dr = dr
+      self.rh = rh
+
+   def setDate(self, date):
+      self.date = date
+      if self.date>=20010101:
+         if self.date < 20010301:
+            self.label='winter'
+         elif 20010301 <= self.date < 20010601:
+            self.label='lente'
+         elif 20010601 <= self.date < 20010901:
+            self.label='zomer'
+         elif 20010901 <= self.date < 20011201:
+            self.label='herfst'
+         else: # from 01-12 to end of year
+            self.label='winter'  
+      else:         
+         if self.date < 20000301:
+            self.label='winter'
+         elif 20000301 <= self.date < 20000601:
+            self.label='lente'
+         elif 20000601 <= self.date < 20000901:
+            self.label='zomer'
+         elif 20000901 <= self.date < 20001201:
+            self.label='herfst'
+         else: # from 01-12 to end of year
+            self.label='winter'
+
+   def removeLabeldata():
+      self.date=None
+      self.label=None
+
+   def measureDistance(self, measurment):
+      return distance.minkowski(
+         numpy.array((
+         self.fg,
+         self.tg,
+         self.tn,
+         self.tx,
+         self.sq,
+         self.dr,
+         self.rh))
+         ,
+         numpy.array((
+         measurment.fg,
+         measurment.tg,
+         measurment.tn,
+         measurment.tx,
+         measurment.sq,
+         measurment.dr,
+         measurment.rh)))
+      
+   def random():
+      return WeatherMeasurement(
+         random.randrange(0, 101),
+         random.randrange(0, 101),
+         random.randrange(0, 101),
+         random.randrange(0, 101),
+         random.randrange(0, 101),
+         random.randrange(0, 101),
+         random.randrange(0, 101),
+         )
+
+   def __str__(self):
+      if (not self.label):
+         return (
+         "\n" + "================" + "\n"
+         "fg: " + str(self.fg) + "\n" +
+         "tg: " + str(self.tg) + "\n" +
+         "tn: " + str(self.tn) + "\n" +
+         "tx: " + str(self.tx) + "\n" +
+         "sq: " + str(self.sq) + "\n" +
+         "dr: " + str(self.dr) + "\n" +
+         "rh: " + str(self.rh) + "\n" +
+         "================" + "\n"
+         )
+
+      return (
+         "\n" + "================" + "\n"
+         "Date: " + str(self.date) + "\n" +
+         "Label: " + self.label + "\n" +
+         "fg: " + str(self.fg) + "\n" +
+         "tg: " + str(self.tg) + "\n" +
+         "tn: " + str(self.tn) + "\n" +
+         "tx: " + str(self.tx) + "\n" +
+         "sq: " + str(self.sq) + "\n" +
+         "dr: " + str(self.dr) + "\n" +
+         "rh: " + str(self.rh) + "\n" +
+         "================" + "\n"
+         )
+   def __add__(self,other):
+      fg = self.fg + other.fg
+      tg = self.tg + other.tg
+      tn = self.tn + other.tn
+      tx = self.tx + other.tx
+      sq = self.sq + other.sq
+      dr = self.dr + other.dr
+      rh = self.rh + other.rh
+      return WeatherMeasurement(fg,tg,tn,tx,sq,dr,rh)
